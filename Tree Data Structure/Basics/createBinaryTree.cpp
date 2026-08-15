c828aee4c0e10d7cc791400d8824c3026677f6af
Ibrahim Ahmad <IbrahimAhmad21006@gmail.com>
2026-08-15 16:16:54 +0530

    Starting Tree Data Structure

---
+++ Tree Data Structure/Basics/createBinaryTree.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main() {
+      
+
+    return 0;
+}
