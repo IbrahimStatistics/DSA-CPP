8ec02641b53e869bca81f3bc0e2569491687e9a3
Ibrahim Ahmad <IbrahimAhmad21006@gmail.com>
2026-08-19 18:47:30 +0530

    Contains Duplicate

---
+++ NeetCode 150/Arrays/contains_duplicate.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> maps;
+        for(int i = 0; i<nums.size(); i++) 
+        {
+            if(maps.find(nums[i])!=maps.end()) {
+                return true;
+            }
+
+            maps[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
